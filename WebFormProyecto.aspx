056790cb5b26798569b406a9788a9666d5fdeb7a
Ness2608 <napereaa@ut.edu.co>
2021-10-03 20:37:13 -0500

    Agregar archivos de proyecto.

---
+++ WebFormProyecto.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormProyecto.aspx.cs" Inherits="proyecto1.WebFormProyecto" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:DropDownList ID="DropDownList1" runat="server" Height="113px" Width="206px">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ WebFormProyecto.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace proyecto1
+{
+    public partial class WebFormProyecto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cCon = "Server = 127.0.0.1; UserID = root@localhost ; Database = prueba; Password = ;";
+            using (MySqlConnection con = new MySqlConnection(cCon))
+
+            {
+                con.Open();
+                const string V = "SELECT * FROM usp_consulta";
+                using (MySqlCommand cmd = new MySqlCommand(V, con))
+
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+
+                    {
+                        DropDownList1.DataSource = reader;
+                        DropDownList1.DataValueField = "DOC_IDENTIDAD";
+                        DropDownList1.DataValueField = "EVENTO";
+                        DropDownList1.DataTextField = "USUARIO";
+                        DropDownList1.DataValueField = "REGISTRO_NOMINA";
+                        DropDownList1.DataBind();
+
+                    }
+                }
+            }    
+        }
+    }
+}
